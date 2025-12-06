82a7845facb707e7b88a09a1ccef029fdca0a5bb
gaz-hutomo <bagaspras690@gmail.com>
2025-12-06 19:23:04 +0700

    Delete toko_online directory

--- toko_online/footer.php
+++
@@ -1,6 +0,0 @@
-</div> <footer class="text-center py-4 text-muted border-top mt-5">
-        &copy; 2025 Tugas E-Commerce - <small>JAVA.STORE</small>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
